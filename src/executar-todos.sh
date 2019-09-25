60ff8f1439ac38cf5d1cd7a13cc513b3e82055ca
Ronald <ronald.drp11@gmail.com>
2019-09-25 15:38:01 -0300

    updated tests

--- src/executar-todos.sh
+++ src/executar-todos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm $1saida_todos_timeout_$2.txt
+rm $1saida_todos.txt
 for file in $1*.txt;
 do
-	echo -n "$file " >> $1saida_todos_timeout_$2.txt
-	time timeout $2 python efficient_transport.py < $file >> $1saida_todos_timeout_$2.txt
+	echo -n "$file " >> $1saida_todos.txt
+	time python efficient_transport.py < $file >> $1saida_todos.txt
 done
