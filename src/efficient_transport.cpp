59280857a378df309e2831fa230fd0a53f81fa52
Ronald <ronald.drp11@gmail.com>
2019-10-02 11:32:28 -0300

    added container lib

--- src/efficient_transport.cpp
+++ src/efficient_transport.cpp
@@ -1,43 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <sstream>
-#include <vector>
-#include <string>
-
-class Container
-{
-private:
-    int height, width;
-    std::vector<int> weights;
-    std::vector<std::vector<int>> initial_config, final_config;
-
-public:
-    Container();
-};
-
-Container::Container()
-{
-    std::cin >> height >> width;
-
-    std::string weights_string;
-    std::cin >> weights_string;
-    std::stringstream iss(weights_string);
-
-    int w;
-    while (iss >> w)
-    {
-        weights.push_back(w);
-    }
-
-    for (auto i : weights)
-        std::cout << i << ' ';
-}
+#include "libs/container.hpp"
 
 int main()
 {
     // Fast reading from input
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
+
+    Container container;
+
+    container.PrintVars();
 
     return 0;
 }
