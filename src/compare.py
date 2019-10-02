f510ce458906b2d5e516316a454ee096277aee8a
Ronald <ronald.drp11@gmail.com>
2019-10-02 20:14:51 -0300

    test script and compare answers python script

--- src/compare.py
+++ src/compare.py
@@ -1,11 +1,17 @@
-with open("../data/saida_todos.txt", "r") as myoutfile, open(
-    "../data/saida_todos_gabriel.txt", "r"
-) as refoutputfile:
+with open("../data/saida_todos_to.txt", "r") as myouttofile, open(
+    "../data/saida_todos.txt", "r"
+) as myoutfile, open("../data/saida_todos_gabriel.txt", "r") as refoutputfile:
     myanswers = {}
     for line in myoutfile:
         filename = line.split(" ")[0].split("/")[-1]
         answer = line.split(" ")[1]
         myanswers[filename] = answer
+
+    mytoanswers = {}
+    for line in myoutfile:
+        filename = line.split(" ")[0].split("/")[-1]
+        answer = line.split(" ")[1]
+        mytoanswers[filename] = answer
 
     refanswers = {}
     for line in refoutputfile:
@@ -24,7 +30,23 @@ with open("../data/saida_todos.txt", "r") as myoutfile, open(
         else:
             diff += 1
 
-    print("-- Results --")
+    print("-- Results without timeout --")
+    print("Same: {}".format(same))
+    print("Diff: {}".format(diff))
+    print("Empty: {}".format(empty))
+
+    same = 0
+    diff = 0
+    empty = 0
+    for filename in mytoanswers.keys():
+        if len(mytoanswers[filename]) == 0:
+            empty += 1
+        elif mytoanswers[filename] == refanswers[filename]:
+            same += 1
+        else:
+            diff += 1
+
+    print("-- Results with timeout 5 --")
     print("Same: {}".format(same))
     print("Diff: {}".format(diff))
     print("Empty: {}".format(empty))
--- src/executar-todos.sh
+++ src/executar-todos.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+rm $1saida_todos_to.txt
+for file in $1inputs/*.txt;
+do
+	echo -n "$file " >> $1saida_todos_to.txt
+	time timeout 5 ./efficient_transport.out < $file >> $1saida_todos_to.txt
+done
+
 rm $1saida_todos.txt
 for file in $1inputs/*.txt;
 do
