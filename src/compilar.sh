72b95a5995eb7f9755c274c9e07b99338f5309f5
Ronald <ronald.drp11@gmail.com>
2019-10-02 12:38:01 -0300

    added libs to compile shell script

--- src/compilar.sh
+++ src/compilar.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -Wall -Wextra -Ofast efficient_transport.cpp -o efficient_transport.out
+g++ -std=c++11 -Wall -Wextra -Ofast libs/*.cpp efficient_transport.cpp -o efficient_transport.out
